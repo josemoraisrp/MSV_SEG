663c0b8306f2ce911ecf77fa1c50ee1cd60c609a
José Morais <RPMORAYS@GMAIL.COM>
2026-05-06 18:57:41 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,20 +1,29 @@
 const form = document.querySelector(".Formulario-fale-conosco")
 const mascara = document.querySelector(".Mascara-formulario")
 
-
-
-
-
+/* ABRIR FORM */
 
 function mostraForm() {
+
     form.style.left = "50%"
-    form.style.transform = "translateX(-50%)"
+    form.style.top = "50%"
+
+    form.style.transform = "translate(-50%, -50%)"
+
     mascara.style.visibility = "visible"
 }
 
-function esconderForm() {
-    form.style.left = "-300px"
-    form.style.transform = "translateX(0)"
-    mascara.style.visibility = "hidden"
+/* FECHAR FORM */
 
+function esconderForm() {
+
+    form.style.left = "-500px"
+
+    mascara.style.visibility = "hidden"
 }
+
+/* VIDEO MAIS LENTO */
+
+const video = document.getElementById("video-fundo")
+
+video.playbackRate = 0.6
